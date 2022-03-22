a9ec114c48bfeccea8d1168aecb2feabda2e23e5
SZF1 A2 <szf1_a2@oktatas.local>
2022-03-22 10:21:09 +0100

    léptetés, stlílus elkészítése

---
+++ javascript.js
@@ -0,0 +1,95 @@
+window.addEventListener("load", init);
+window.addEventListener("click", galeria);
+
+
+function ID(elem) {
+    return document.getElementById(elem);
+}
+
+var kepeim = [
+    {
+    eleres: "kepek/kep01.jpg",
+    cim: "Állatok",
+    leiras: "Kecskék",
+    }
+    ,
+    {
+    eleres: "kepek/kep02.jpg",
+    cim: "Japán fa",
+    leiras: "Szép sétány"
+    }
+    ,
+    {
+    eleres: "kepek/kep03.jpg",
+    cim: "Sárga virágok",
+    leiras: "Szép mezőföld"
+    }
+    ,
+    {
+    eleres: "kepek/kep04.jpg",
+    cim: "Tájkép",
+    leiras: "Fent a magasban!"
+    }
+    ,
+    {
+    eleres: "kepek/kep05.jpg",
+    cim: "Az út mellet",
+    leiras: "Szép fehér virágok az út mellet"
+    }
+]
+
+var kepindex = 0;
+function galeria(){
+    var txt = "";
+    for (let index = 0; index < kepeim.length; index++) {
+        txt += `<div class="kep" >
+        <img id="${index}" src="${kepeim[index].eleres}" alt="${kepeim[index].cim}" ></div>`
+        
+    }
+    ID("inp").innerHTML=txt;
+    for (let index = 0; index < kepeim.length; index++) {
+        ID(index).addEventListener("click",kepkivalasztas)
+        document.getElementsByClassName("kep")[index].style.border="5px solid red";
+        document.getElementsByClassName("kep")[index].className+="kepekformazasa";
+    }
+
+}
+
+function kepkivalasztas() {
+    console.log(event.target.id);
+    kepindex=Number(event.target.id);
+    megjelenit();
+
+}
+
+function init() {
+    megjelenit();
+    galeria();
+    ID("balra").addEventListener("click", balra);
+    ID("jobbra").addEventListener("click", jobbra);
+}
+function megjelenit() {
+    ID("kivalasztott").src = kepeim[kepindex].eleres;
+    ID("kivalasztott").alt = kepeim[kepindex].cim;
+
+}
+function balra() {
+    if(kepindex <= 0){
+        kepindex = kepeim.length-1;
+    }
+    else{
+        kepindex -= 1;
+    }
+    console.log(kepindex);
+    megjelenit(kepindex);
+}
+function jobbra() {
+    if(kepindex >= kepeim.length-1){
+        kepindex = 0;
+    }
+    else{
+        kepindex ++;
+    }
+    console.log(kepindex);
+    megjelenit(kepindex)
+}
